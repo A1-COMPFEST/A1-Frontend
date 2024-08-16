c96ac1dd22435a83849eeff9c194558d19aa68da
Danan <narayanmade578@gmail.com>
2024-08-16 21:34:31 +0700

    fix: adjust e2e test file

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,10 +1,33 @@
 import { test, expect } from '@playwright/test';
 
+const baseUrl = "https://a1-frontend-brown.vercel.app";
+
 test('home navigation', async ({ page }) => {
 
-    await page.goto(`${process.env.BASE_URL}`);
+    await page.goto(baseUrl);
     const exploreButton = await page.locator('text=Explore Courses');
     await exploreButton.click();
-    await expect(page).toHaveURL(`${process.env.BASE_URL}/explore`);
+    await expect(page).toHaveURL(baseUrl + "/explore");
 
+});
+
+test('login navigation', async ({ page }) => {
+
+    await page.goto(baseUrl);
+    const loginButton = await page.locator('text=sign in');
+    await loginButton.click();
+    await expect(page).toHaveURL(baseUrl + "/auth/sign-in");
+
+});
+
+test('register navigation', async ({ page }) => {
+
+    await page.goto(baseUrl);
+    const loginButton = await page.locator('text=sign in');
+    await loginButton.click();
+    await expect(page).toHaveURL(baseUrl + "/auth/sign-in");
+
+    const registerButton = await page.locator('text=sign up');
+    await registerButton.click();
+    await expect(page).toHaveURL(baseUrl + "/auth/sign-up");
 });
